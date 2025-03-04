590b8a5577a5b0211fed22370e26c29215cdbda8
ahmadshoev <ahmadshoevg@gmail.com>
2025-03-04 07:10:09 +0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
       'lg': '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1440px',
+      'xl': '1240px',
       // => @media (min-width: 1280px) { ... }
 
       '2xl': '1536px',
